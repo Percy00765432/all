4fa2888a636db3d78a7cbd2734f01733364bbad8
Prathik <prathikkb68@gmail.com>
2024-11-19 19:43:59 +0530

    Initial commit

--- grocery.c
+++ grocery.c
@@ -1,19 +1,26 @@
 #include<stdio.h>
-int main()
-{
-int x,i,a,b,q;
-float tax,gst,total,itmp,result;
-printf("enter the no of items");
-scanf("%d",&i);
-for(x=1;x<=i;x++)
-{
-printf("enter the item %d cost and quantity: ",x);
-scanf("%f %d",&itmp,&q);
-result=itmp*q;
-total=total+result;
-}
-printf("the total cost is:%.2f",total);
+int main(){
+    int i, q, x;
+    float tax, gst, total = 0, itmp, result;
 
- return 0;
-  
- }
+    printf("enter the no of items: ");
+    scanf("%d", &i);
+
+    printf("Item\tTotal\tPrice/Item\tItem Bill\n");
+    for(x = 1; x <= i; x++){
+        printf("enter the item%d cost and quantity: ", x);
+        scanf("%f %d", &itmp, &q);
+        result = itmp * q;
+        total += result;
+        printf("Item%d\t%d\t%.2f\t\t%.2f\n", x, q, itmp, result);
+    }
+
+    tax = total * 0.05;
+    gst = total + tax;
+
+    printf("\nTotal\t\t\t\t%.2f\n", total);
+    printf("Tax\t\t\t\t%.2f\n", tax);
+    printf("Total Amount (with GST)\t\t%.2f\n", gst);
+
+    return 0;
+}
