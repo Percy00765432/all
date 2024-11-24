75f2bbfcc8fc48501c8f1a5359f6c5c06c6a4e6c
Percy <prathikkb68@gmail.com>
2024-11-24 16:43:07 +0530

    Update

---
+++ calc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+int a,b;
+float d;
+char op;
+printf("Enter the operation: ");
+scanf("%d%c%d",&a,&op,&b);
+switch(op)
+{
+case '+':
+printf("The sum of two numbers are: %d\n",a+b);
+break;
+case '-':
+printf("The difference of the two numbers are: %d\n",a-b);
+break;
+case '*':
+printf("The product of two numbers are: %d\n",a*b);
+case '/':
+if(b!=0){
+d=(float)a/b;
+printf("The division of the two numbers are: %f\n",d);
+}
+else
+printf("Invalid\n");
+break;
+default:
+printf("Invalid Input\n");
+}
+}
+
+
